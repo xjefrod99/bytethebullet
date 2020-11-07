8bf1d5de03d5e770c6a2b5f72f62c5cf9c720d71
xjefrod99 <jeffrodriguez4321@gmail.com>
2020-11-07 15:22:19 -0500

    Created contact table

--- bulletinboard/website/models.py
+++ bulletinboard/website/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.model):
+    name = models.CharField(max_length = 30)
+    contacts = models.ManyToManyField("self", null= True, blank = True)
+    #name.contacts.all()
