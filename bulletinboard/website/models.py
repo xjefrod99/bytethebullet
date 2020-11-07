301a0064b0e42b58968e94294b5e83d26368da65
Simon Scharf <simonmscharf@Simons-MacBook-Air.local>
2020-11-07 13:48:15 -0800

    added person class

--- bulletinboard/website/models.py
+++ bulletinboard/website/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Announcement(models.Model):
     text = models.CharField(max_length=32,default='')
+
+
+class Person(models.Model):
+    text = models.CharField(max_length=32,default='')
+    contacts = models.ManyToManyField("self", blank=True)
