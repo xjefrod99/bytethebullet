d01dfeb124ce37d81cb924796276ae3688b49a23
SimonScharf <65061980+SimonScharf@users.noreply.github.com>
2020-11-08 00:35:26 -0800

    Update forms.py

--- bulletinboard/website/forms.py
+++ bulletinboard/website/forms.py
@@ -12,3 +12,7 @@ class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ('name', 'contacts')
+    contacts = forms.ModelMultipleChoiceField(
+        queryset=Person.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
